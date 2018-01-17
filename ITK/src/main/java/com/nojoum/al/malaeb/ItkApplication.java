7030b499fd496cd21415af41fe6f1d9d660546dc
Utilisateur <Utilisateur@DESKTOP-Q76N1AH>
2018-01-17 11:51:26 +0100

    creation project

---
+++ ITK/src/main/java/com/nojoum/al/malaeb/ItkApplication.java
@@ -0,0 +1,12 @@
+package com.nojoum.al.malaeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItkApplication.class, args);
+	}
+}
